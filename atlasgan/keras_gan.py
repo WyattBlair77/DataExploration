8ccd3bf5e75badae1b89aabab2e26dc86db88351
Wyatt Blair <wyattblair@berkeley.edu>
2019-04-04 22:26:04 -0700

    updates

--- atlasgan/keras_gan.py
+++ atlasgan/keras_gan.py
@@ -53,17 +53,19 @@ class GAN():
     def build_generator(self):
 
         model = Sequential()
-
-        model.add(Dense(256, input_dim=self.latent_dim))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
+        
+        model.add(Conv2D(256, (5,5), input_shape = [self.img_rows, self.img_cols, 1], activation = 'relu'))
+        
+#         model.add(Dense(256, input_dim=self.latent_dim))
+#         model.add(LeakyReLU(alpha=0.2))
+#         model.add(BatchNormalization(momentum=0.8))
         model.add(Dense(512))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.8))
         model.add(Dense(1024))
         model.add(LeakyReLU(alpha=0.2))
         model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(Dense(np.prod(self.img_shape), activation='sigmoid'))
         model.add(Reshape(self.img_shape))
 
         model.summary()
